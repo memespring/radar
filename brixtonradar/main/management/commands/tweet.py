8ec2de5385a3ad6ecaab9ba37c33a2e2b81192cf
Richard <richard@memespring.co.uk>
2013-02-09 14:09:42 +0000

    req

--- brixtonradar/main/management/commands/tweet.py
+++ brixtonradar/main/management/commands/tweet.py
@@ -1,14 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
 import twitter
+import settings
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print "Singing in to Twitter"
-        api = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
-consumer_secret=TWITTER_CONSUMER_SECRET, access_token_key=TWITTER_ACCESS_TOKEN_KEY, access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+        api = twitter.Api(consumer_key=settings.TWITTER_CONSUMER_KEY,
+consumer_secret=settings.TWITTER_CONSUMER_SECRET, access_token_key=settings.TWITTER_ACCESS_TOKEN_KEY, access_token_secret=settings.TWITTER_ACCESS_TOKEN_SECRET)
 
         print "Posting tweet"
-        status = api.PostUpdate('another test')
+        status = api.PostUpdate('yet another test 1')
 
         print "Tweet was: %s" % status
