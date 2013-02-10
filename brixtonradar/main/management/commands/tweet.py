b2ba7bf89128eee0fd25a1518773118509e64254
Richard <richard@memespring.co.uk>
2013-02-10 21:36:48 +0000

    hashtag

--- brixtonradar/main/management/commands/tweet.py
+++ brixtonradar/main/management/commands/tweet.py
@@ -19,7 +19,7 @@ consumer_secret=settings.TWITTER_CONSUMER_SECRET, access_token_key=settings.TWIT
         for alert in alerts:
             print "Posting tweet"
             url = '%s/%s' % (settings.BASE_URL, reverse('event', args=[alert.event.pk]))
-            status = api.PostUpdate(alert.event.message + ' ' + url, latitude=alert.event.lat, longitude=alert.event.lng)
+            status = api.PostUpdate(alert.event.message + ' ' + url + " #brixton", latitude=alert.event.lat, longitude=alert.event.lng)
             print "Tweet sent %s" % alert.event.message
             time.sleep(settings.SLEEP_BETWEEN_TWEETS)
             print "Waiting before sending next tweet"
